32d32819c94e4787810af14d2a6dc0b33611ee8d
FKtPp <kai.fktpp@gmail.com>
2015-06-12 01:09:02 +0800

    Chapter 4 continued
    Read the wikipedia article to understand fold

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -10,3 +10,46 @@ foldl (+) 0 (1:2:3:[])
           == foldl (+) (((0 + 1) + 2) + 3) []
           ==           (((0 + 1) + 2) + 3)
 
+foldr :: (a -> b -> b) -> b -> [a] -> b
+
+foldr step zero (x:xs) = step x (foldr step zero xs)
+foldr _    zero []     = zero
+
+foldr (+) 0 (1:2:3:[])
+          == 1 +           foldr (+) 0 (2:3:[])
+          == 1 + (2 +      foldr (+) 0 (3:[])
+          == 1 + (2 + (3 + foldr (+) 0 []))
+          == 1 + (2 + (3 + 0))
+
+
+-- file: ch04/Fold.hs
+1 : (2 : (3 : []))
+1 + (2 + (3 + 0 ))
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter p []   = []
+filter p (x:xs)
+    | p x       = x : filter p xs
+    | otherwise = filter p xs
+
+-- file: ch04/Fold.hs
+myFilter p xs = foldr step [] xs
+    where step x ys | p x       = x : ys
+                    | otherwise = ys
+
+
+-- file: ch04/Fold.hs
+myMap :: (a -> b) -> [a] -> [b]
+
+myMap f xs = foldr step [] xs
+    where step x ys = f x : ys
+
+
+-- file: ch04/Fold.hs
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+
+myFoldl f z xs = foldr step id xs z
+    where step x g a = g (f a x)
+-- Read
+-- http://en.wikipedia.org/wiki/Fold_%28higher-order_function%29
+
